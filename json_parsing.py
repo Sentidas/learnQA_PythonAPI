2365c55191f86b8345a7a62839400a46c52c0129
Lena <sentidas@yandex.ru>
2021-08-19 17:30:49 +0300

    update__Ex5: Парсинг JSON

--- json_parsing.py
+++ json_parsing.py
@@ -3,10 +3,14 @@ import json
 string_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 
 obj = json.loads(string_text)
-print(obj['messages'])
+key = 'messages'
 
-second_message = obj['messages'][1]
-print(second_message)
+if key in obj:
+    print(obj['messages'])
+    second_message = obj['messages'][1]
+    print(f"Текст второго сообщения - {second_message}")
+else:
+    print(f"Ключа {key} в JSON нет")
 
 
 
